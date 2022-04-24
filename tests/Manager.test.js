09306c71f44e5ade7a6a0c684ad18c1cf967241e
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-04-24 15:32:30 +0800

    Update Manager test and classes

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,24 +1,39 @@
-
-describe('Employee class !', () => {
+const Manager = require('../lib/Manager.js');
+describe('Manager class !', () => {
+    const name = 'Vinitha';
+    const email = 'sreevinithaa@gmail.com';
+    const id = 1;
+    const officenumber="sdf"
     describe("Initialization", () => {
-        it("getRole() function should return role!", () => {
-            // Arrange
+        it("should create an object with a 'name','email','id' and 'officeNumber' property set to the name,email, id and officeNumber argument provided when called with the 'new' keyword.", () => {
           
+            // Act
+            const obj = new Manager(id,name,email,officenumber);
+      
             // Assert
-            expect(true).toEqual(true);
+            expect(obj).toBeInstanceOf(Manager);
+          });
+          it("Should throw exception if fail to pass four arguments", () => {
+          
+            const obj = () => new Manager(email);
+           
+      
+            // Assert
+            expect(obj).toThrowError(Error);
           });
     });
-    describe("getName()", () => {
-       
-    });
-    describe("getId()", () => {
-       
-    });
-    describe("getEmail()", () => {
-       
-    });
     describe("getRole()", () => {
+        it("getRole() function should return role!", () => {
+            // Arrange
+           
+            const role=`Manager`;
+            // Act
+            const obj = new Manager(id,name,email,officenumber);
       
+            // Assert
+            expect(obj.getRole()).toEqual(role);
+          });
     });
+   
 
 });
