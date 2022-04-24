4361b74c8f997783a44ad132e37ff129157ceb61
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-04-24 15:45:14 +0800

    Update Intern class and test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,24 +1,50 @@
-
-describe('Employee class !', () => {
+const Intern = require('../lib/Intern');
+describe('Intern class !', () => {
+    const name = 'Vinitha';
+    const email = 'sreevinithaa@gmail.com';
+    const id = 1;
+    const school="sreevinithaa"
     describe("Initialization", () => {
-        it("getRole() function should return role!", () => {
-            // Arrange
+        it("should create an object with a 'name','email','id' and 'school' property set to the name,email, id and school argument provided when called with the 'new' keyword.", () => {
           
+            // Act
+            const obj = new Intern(id,name,email,school);
+      
             // Assert
-            expect(true).toEqual(true);
+            expect(obj).toBeInstanceOf(Intern);
+          });
+          it("Should throw exception if fail to pass four arguments", () => {
+          
+            const obj = () => new Intern(email);
+           
+      
+            // Assert
+            expect(obj).toThrowError(Error);
           });
     });
-    describe("getName()", () => {
-       
-    });
-    describe("getId()", () => {
-       
-    });
-    describe("getEmail()", () => {
-       
-    });
     describe("getRole()", () => {
+        it("getRole() function should return role!", () => {
+            // Arrange
+           
+            const role=`Intern`;
+            // Act
+            const obj = new Intern(id,name,email,school);
       
+            // Assert
+            expect(obj.getRole()).toEqual(role);
+          });
+    });
+    describe("getschool()", () => {
+        it("getschool() function should return school property value!", () => {
+            // Arrange
+           
+            
+            // Act
+            const obj = new Intern(id,name,email,school);
+      
+            // Assert
+            expect(obj.school).toEqual(school);
+          });
     });
 
 });
